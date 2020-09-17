6244ffcf1c8214395a23b98a256c13e26f93a302
emwkempen <39294307+emwkempen@users.noreply.github.com>
2020-09-17 18:04:17 +0200

    Add files via upload

---
+++ MoistureSensor.ino
@@ -0,0 +1,27 @@
+#define ANA 26
+#define DIGI 25
+
+double analogValue = 0.0;
+int digitalValue = 0;
+double analogVolts = 0.0;
+unsigned long timeHolder = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ANA, INPUT);
+  pinMode(DIGI, INPUT);
+  Serial.begin(115200);
+  Serial.println("Paolo Test");
+}
+
+void loop() {
+  analogValue = analogRead(ANA);
+  digitalValue = digitalRead(DIGI);
+  // Serial data
+  Serial.print("Analog raw: ");
+  Serial.println(analogValue);
+  Serial.print("Digital raw: ");
+  Serial.println(digitalValue);
+  Serial.println(" ");
+  delay(1000);
+}
