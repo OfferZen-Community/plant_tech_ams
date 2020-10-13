de4b5aee631ce696277fbb266184396e04024b16
emwkempen <39294307+emwkempen@users.noreply.github.com>
2020-10-13 21:36:02 +0200

    Add files via upload

---
+++ BlinkESP.ino
@@ -0,0 +1,26 @@
+/*
+  Blink an ESP8266 NodeMCU board
+  Turns flashes an LED for one second delay.
+  
+  This program needs no circuit. It will use a built-in LED
+  
+  by Eng. Alaa Al Nassan
+ */
+
+
+# define LED D4 // Use built-in LED which connected to D4 pin or GPIO 2
+
+void setup() {
+  pinMode(LED, OUTPUT);     // Initialize the LED as an output
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);  // Turn the LED off because the LED is active low
+  delay(1000);            // Wait a second
+  digitalWrite(LED, LOW);   // Turn the LED on because the LED is active low
+  delay(1000);              // Wait a second
+}
+
+
+
+
