7260d30632fd9a78ca6620f42e2ab981e7a6b98b
Marius Potgieter <mariuspotgieter@gmail.com>
2020-11-25 18:36:32 +0100

    Added serial to blinky

--- BlinkESP.ino
+++ BlinkESP.ino
@@ -12,12 +12,15 @@
 
 void setup() {
   pinMode(LED, OUTPUT);     // Initialize the LED as an output
+  Serial.begin(115200);
 }
 
 void loop() {
   digitalWrite(LED, HIGH);  // Turn the LED off because the LED is active low
+  Serial.println("ON");
   delay(1000);            // Wait a second
   digitalWrite(LED, LOW);   // Turn the LED on because the LED is active low
+  Serial.println("OFF");
   delay(1000);              // Wait a second
 }
 
