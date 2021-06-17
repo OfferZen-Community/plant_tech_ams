e2fd2b406987b4eb5b5cbc08ad91ff007412ca6f
Paolo Brizzolari <paolo.brizzolari@basebone.com>
2021-06-17 19:08:15 +0200

    Updated Moisture Sensor

--- arduino/MoistureSensor/MoistureSensor.ino
+++ arduino/MoistureSensor/MoistureSensor.ino
@@ -1,5 +1,6 @@
 #define ANA A0
 #define DIGI D5
+#define SENSOR D7
 
 double analogValue = 0.0;
 int digitalValue = 0;
@@ -10,13 +11,17 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(ANA, INPUT);
   pinMode(DIGI, INPUT);
+  pinMode(SENSOR, OUTPUT);
   Serial.begin(115200);
   Serial.println("Test");
 }
 
 void loop() {
+  digitalWrite(SENSOR, HIGH);
+  delay(100);
   analogValue = analogRead(ANA);
   digitalValue = digitalRead(DIGI);
+  digitalWrite(SENSOR, LOW);
   // Serial data
   Serial.print("Analog raw: ");
   Serial.println(analogValue);
